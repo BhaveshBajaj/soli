a0423d128b02669d28ee47f015f398243c313696
Bhavesh Bajaj <bhaveshbajajb11@gmail.com>
2022-09-13 18:01:10 +0530

    added n sol

---
+++ n.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ERC20 is IERC20 {
+    uint public totalSupply = 100000;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name = "Solidity by Example";
+}
+//sssssss;opajp;oaj;oscjco;cjsco;
